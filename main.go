26c9026ae2665eb53f2ae35a5e63d09d9a612bf3
Ilya Rayko <mylntsa.ilya.63@gmail.com>
2023-06-14 23:00:08 +0300

    :necktie: connect to kube cluster

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func ReadAccess(filename string) string {
@@ -63,10 +71,41 @@ func GetOpen(token string) []int {
 	return result
 }
 
+func KubeConnect() error {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	return nil
+}
+
 func main() {
 	token := ReadAccess("access.json")
 	fmt.Println(token)
 	danglins := GetOpen(token)
 	fmt.Println(danglins)
 
+	if err = KubeConnect(); err != nil {
+		log.Fatal(err)
+	}
 }
